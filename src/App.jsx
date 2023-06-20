fe9ca8f4098478f70af332123678dcdd0bd87abc
Nodari Kobalia <qobalia8@gmail.com>
2023-06-20 22:54:10 +0400

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
 import { Component } from 'react';
 import './App.css';
+import ToDo from './components/ToDo';
 
 
 class App extends Component{
-
-    state = {
-      todo: '',
-      done: ''
-    }
-
-    onChange=(event)=>{
-      const value = event.target.value
-      this.setState({
-        todo: value
-      })
-    }
-
-    render(){
-      return(
-        <div className='container'>
-          <input type="text" placeholder='type task' onChange={this.onChange} />
-          <div className='block'>
-            <div className='todo_container'>
-              <p>{this.state.todo}</p>
-              <button>Done</button>
-            </div>
-            <div className='done_container'>
-              <p>Done</p>
-              <button>mark as a undone</button>
-              <button>Remove</button>
-            </div>
-          </div>
-        </div>
-      )
-    }
+  render(){
+    return(
+      <div>
+      <ToDo/>
+    </div>
+    )
+  }
 }
 
 export default App
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import '../App.css';
+
+class ToDo extends Component{
+    state = {
+        todo: '',
+        done: ''
+      }
+  
+      onChange=(event)=>{
+        const value = event.target.value
+        this.setState({
+          todo: value
+        })
+      }
+      
+      render(){
+        return(
+          <div className='container'>
+            <input type="text" placeholder='type task' onChange={this.onChange} />
+            <div className='block'>
+              <div className='todo_container'>
+                <p>{this.state.todo}</p>
+                <button>Done</button>
+              </div>
+              <div className='done_container'>
+                <p>Done</p>
+                <button>mark as a undone</button>
+                <button>Remove</button>
+              </div>
+            </div>
+          </div>
+        )
+      }
+}
+
+export default ToDo
